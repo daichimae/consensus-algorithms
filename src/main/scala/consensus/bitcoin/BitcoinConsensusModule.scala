386804262d611c8d35df202325d12d47e07d3e76
daichimae <speaknoevil84194@gmail.com>
2018-02-27 16:53:52 -0500

    Implemented the feeDistribution, genesisData and calculateBlockHash methods.

--- src/main/scala/consensus/bitcoin/BitcoinConsensusModule.scala
+++ src/main/scala/consensus/bitcoin/BitcoinConsensusModule.scala
@@ -1,8 +1,12 @@
 package consensus.bitcoin
 
+import java.math.BigInteger
+import java.security.MessageDigest
+
 import scorex.account.{Account, PrivateKeyAccount}
 import scorex.block.{Block, BlockField}
 import scorex.consensus.ConsensusModule
+import scorex.crypto.hash.{Blake256, SecureCryptographicHash}
 import scorex.transaction.TransactionModule
 import scorex.utils.ScorexLogging
 
@@ -18,14 +22,27 @@ class BitcoinConsensusModule extends ConsensusModule[BitcoinConsensusBlockData]
   with ScorexLogging {
 
   implicit val consensusModule = this
-  private def calculateBlockHash(block: Block): BigInt = ???
+
+  /**
+    * Calculate the hash value of a block.
+    *
+    * @param block
+    * @return
+    */
+  private def calculateBlockHash(block: Block): BigInt = {
+    new BigInteger(1, MessageDigest.getInstance("SHA-256").digest(block.bytes))
+  }
 
   override def isValid[TransactionalBlockData]
-  (block: Block)(implicit transactionModule: TransactionModule[TransactionalBlockData]): Boolean = ???
+  (block: Block)(implicit transactionModule: TransactionModule[TransactionalBlockData]): Boolean = true
 
-  override def feesDistribution(block: Block): Map[Account, Long] = ???
+  override def feesDistribution(block: Block): Map[Account, Long] = {
+    val forger = block.consensusModule.generators(block).ensuring(_.size == 1).head
+    val fee = block.transactions.map(_.fee).sum
+    Map(forger -> fee)
+  }
 
-  override def generators(block: Block): Seq[Account] = ???
+  override def generators(block: Block): Seq[Account]
 
   override def blockScore(block: Block)(implicit transactionModule: TransactionModule[_]): BigInt = ???
 
@@ -36,7 +53,11 @@ class BitcoinConsensusModule extends ConsensusModule[BitcoinConsensusBlockData]
 
   override def parseBytes(bytes: Array[Byte]): Try[BlockField[BitcoinConsensusBlockData]] = ???
 
-  override def genesisData: BlockField[BitcoinConsensusBlockData] = ???
+  override def genesisData: BlockField[BitcoinConsensusBlockData] =
+    BitcoinConsensusBlockField(new BitcoinConsensusBlockData {
+      override val nonce: Long = -1
+      override val target: BigInt = new BigInteger("00000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16)
+    })
 
   override def formBlockData(data: BitcoinConsensusBlockData): BlockField[BitcoinConsensusBlockData] = ???
 }
